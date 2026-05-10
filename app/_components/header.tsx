f899ca43763aa0bbffb56688a93a5e721d838799
matheushssilvaa <msxatendimento@outlook.com.br>
2026-05-10 08:17:19 -0300

    feat: add Header component and banner

---
+++ app/_components/header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+import { Card, CardContent } from "./ui/card"
+import { Button } from "./ui/button"
+import { MenuIcon } from "lucide-react"
+
+const Header = () => {
+	return (
+		<Card>
+			<CardContent className="flex flex-row justify-between items-center">
+				<Image src="/logo.png" width={120} height={18} alt="FSW Barber" loading="eager" />
+				<Button size="icon" variant="outline">
+					<MenuIcon />
+				</Button>
+			</CardContent>
+		</Card>
+	)
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
-import Image from "next/image";
+import { SearchIcon } from "lucide-react";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
+import { Input } from "./_components/ui/input";
+import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-			<Button>Click here</Button>
+		<div>
+			<Header />
+			<div className="p-5">
+				<h2 className="font-xl font-bold">Olá, Cliente!</h2>
+				<p>Segunda-feira, 05 de agosto</p>
+
+				<div className="flex items-center gap-2 mt-6">
+					<Input placeholder="Pesquisar..." />
+					<Button>
+						<SearchIcon />
+					</Button>
+				</div>
+
+				<div className="relative mt-6 w-full h-37.5">
+					<Image
+						src="/Banner01.png"
+						fill
+						className="object-cover rounded-xl"
+						alt="Banner"
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
