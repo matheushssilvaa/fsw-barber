1d54de6ea03d7e158e11fe27109b8b21f0a8d2f6
matheushssilvaa <msxatendimento@outlook.com.br>
2026-05-11 11:49:28 -0300

    feat: add booking item

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -6,7 +6,7 @@ import { MenuIcon } from "lucide-react"
 const Header = () => {
 	return (
 		<Card>
-			<CardContent className="flex flex-row justify-between items-center">
+			<CardContent className="flex flex-row justify-between items-center p-5">
 				<Image src="/logo.png" width={120} height={18} alt="FSW Barber" loading="eager" />
 				<Button size="icon" variant="outline">
 					<MenuIcon />
